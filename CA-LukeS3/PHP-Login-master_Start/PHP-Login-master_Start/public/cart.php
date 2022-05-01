40c4119c75e160589b87eb2a0d403f0a33c982ec
Senan999 <senanmangan12@gmail.com>
2022-05-01 21:43:06 +0100

    Added cart

---
+++ CA-LukeS3/PHP-Login-master_Start/PHP-Login-master_Start/public/cart.php
@@ -0,0 +1,43 @@
+<?php
+//Setting session start
+session_start();
+
+$total=0;
+
+//Database connection, replace with your connection string.. Used PDO
+$conn = new PDO("mysql:host=localhost;dbname=test", 'root', 'rm1tagE');
+
+
+//get action string
+$action = isset($_GET['action'])?$_GET['action']:"";
+
+//Add to cart
+if($action=='addcart' && $_SERVER['REQUEST_METHOD']=='POST') {
+
+    //Finding the product by code
+    $query = "SELECT * FROM productList WHERE id=:id";
+    $stmt = $conn->prepare($query);
+    $stmt->bindParam('id', $_POST['id']);
+    $stmt->execute();
+    $product = $stmt->fetch();
+
+    $currentQty = $_SESSION['productName'][$_POST['id']]['qty']+1; //Incrementing the product qty in cart
+    $_SESSION['productName'][$_POST['id']] =array('qty'=>$currentQty,'name'=>$product['name'],'price'=>$product['price']);
+    $product='';
+    header("Location:cart.php");
+}
+
+//Empty All
+if($action=='emptyall') {
+    $_SESSION['products'] =array();
+    header("Location:shopping-cart.php");
+}
+
+//Empty one by one
+if($action=='empty') {
+    $id = $_GET['id'];
+    $productName = $_SESSION['products'];
+    unset($products[$id]);
+    $_SESSION['products']= $productName;
+    header("Location:cart.php");
+}
